75407d2c8902a9a53bc31de2ccde53446f43cc31
blacksmithop <angstycoder101@gmail.com>
2020-10-02 18:28:54 +0530

    scraping data with bs4

--- dataset/datasetGen.py
+++ dataset/datasetGen.py
@@ -10,7 +10,7 @@ def Tag_To_Url(base: str, tag: str, page: int):
         content = response.read()
     tag_url = findall(r'<h3><a href="(.*?)" class', str(content))[1:]
     tag_url = [f'{base}{link}' for link in tag_url]
-    #return list of urls for that tag
+    # return list of urls for that tag
     yield tag_url
 
 
@@ -24,24 +24,24 @@ def Url_To_Data(url: str):
     source_verified = False
     verified_question = soup.find("div", {"class": "answer accepted-answer"})
     if verified_question:
-        source_verified = True #is an accepted answer
+        source_verified = True  # is an accepted answer
 
     manual_verified = False
 
     answer = soup.find("div", {"class": "answercell post-layout--right"})
     code_blocks = answer.find_all('code')
 
-    code_blocks = [code.text.replace('\\n', '') for code in code_blocks if len(code.text.split())!=1]
-    answer = '\n'.join(code_blocks) #concatenated list of commands (code) as the answer
+    code_blocks = [code.text.replace('\\n', '') for code in code_blocks if len(code.text.split()) != 1]
+    answer = '\n'.join(code_blocks)  # concatenated list of commands (code) as the answer
 
-    if source_verified: #getting answer from verified answers (only)
+    if source_verified:  # getting answer from verified answers (only)
         answer_id = verified_question.get('id')
         answer_id = findall(r'\d+', answer_id)[0]
-        url = f"https://{platform}/a/{answer_id}" #answer share link
+        url = f"https://{platform}/a/{answer_id}"  # answer share link
 
-    final_answer = None #defaults to None
+    final_answer = None  # defaults to None
 
-    #answer schema as a dict - to be changed to csv
+    # answer schema as a dict - to be changed to csv
     post = {
         'question': question,
         'answer': answer,
@@ -52,9 +52,10 @@ def Url_To_Data(url: str):
     }
     return post
 
-#Parameters
 
-platform_to_tag = { #tags and platforms to consider
+# Parameters
+
+platform_to_tag = {  # tags and platforms to consider
     "askubuntu": [
         "mysql", "command-line", "apt"
     ],
@@ -62,12 +63,8 @@ platform_to_tag = { #tags and platforms to consider
         "aiohttp", "python", "django"
     ]
 }
-#test
-platform_to_tag = {
-    "askubuntu": ["apt"]
-}
 
-PAGE_MAX = 3 #number of pages to consider
+PAGE_MAX = 3  # number of pages to consider
 
 
 def collect_urls():
@@ -76,7 +73,7 @@ def collect_urls():
         tags = platform_to_tag[platform]
         for tag in tags:
             tag_urls[tag] = []
-            for i in range(1, PAGE_MAX+1): #PAGE_MAX number of list of urls per iteration
+            for i in range(1, PAGE_MAX + 1):  # PAGE_MAX number of list of urls per iteration
                 for url in Tag_To_Url(base=platform, tag=tag, page=i):
                     tag_urls[tag].extend(url)
             print(f"Found {len(tag_urls[tag])} urls for tag {tag} in {platform} searching {PAGE_MAX} pages")
@@ -84,19 +81,15 @@ def collect_urls():
 
 
 def data_from_url(tag_urls: dict):
-    dataset = []
+    _dataset = []
     for tag in tag_urls:
-        urls = tag_urls[tag]
-        for url in urls:
-            dataset.append(Url_To_Data(url=url))
+        _urls = tag_urls[tag]
+        for url in _urls:
+            _dataset.append(Url_To_Data(url=url))
         print(f"Collected data from {len(urls)} urls with tag {tag}")
-    return dataset
+    return _dataset
 
 
 if __name__ == '__main__':
     urls = collect_urls()
     dataset = data_from_url(tag_urls=urls)
-
-
-
-
