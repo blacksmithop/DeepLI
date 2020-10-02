cfffd93c9eb69115618bc55098658e37942ded78
blacksmithop <angstycoder101@gmail.com>
2020-10-02 18:27:10 +0530

    scraping data with bs4

--- dataset/datasetGen.py
+++ dataset/datasetGen.py
@@ -14,39 +14,47 @@ def Tag_To_Url(base: str, tag: str, page: int):
     yield tag_url
 
 
-def Link_To_Data(urls: list):
-    url = urls[0] #test
+def Url_To_Data(url: str):
     platform = search('https?://([A-Za-z_0-9.-]+).com*', url).group(1)
-    print(f"Platform: {platform}")
     with request.urlopen(url) as response:
         content = response.read()
     soup = BeautifulSoup(str(content), 'html.parser')
-    question = soup.find("a", {"class": "question-hyperlink"})
-    print(f"Question: {question.text}") #title of the question
+    question = soup.find("a", {"class": "question-hyperlink"}).text
 
     source_verified = False
     verified_question = soup.find("div", {"class": "answer accepted-answer"})
     if verified_question:
         source_verified = True #is an accepted answer
-    print(f"Source_Verified: {source_verified}")
 
     manual_verified = False
-    print(f"Manual_Verified: {manual_verified}") #to be done later
 
     answer = soup.find("div", {"class": "answercell post-layout--right"})
     code_blocks = answer.find_all('code')
-    print("Answer: (commands only)")
+
     code_blocks = [code.text.replace('\\n', '') for code in code_blocks if len(code.text.split())!=1]
-    for code in code_blocks:
-        print(code)
-    if source_verified:
+    answer = '\n'.join(code_blocks) #concatenated list of commands (code) as the answer
+
+    if source_verified: #getting answer from verified answers (only)
         answer_id = verified_question.get('id')
         answer_id = findall(r'\d+', answer_id)[0]
-        url = f"https://{platform}/a/{answer_id}"
-    print(f"Url: {url}")
-        
+        url = f"https://{platform}/a/{answer_id}" #answer share link
 
-platform_to_tag = {
+    final_answer = None #defaults to None
+
+    #answer schema as a dict - to be changed to csv
+    post = {
+        'question': question,
+        'answer': answer,
+        'source_verified': source_verified,
+        'manual_verified': manual_verified,
+        'url': url,
+        'final_answer': final_answer
+    }
+    return post
+
+#Parameters
+
+platform_to_tag = { #tags and platforms to consider
     "askubuntu": [
         "mysql", "command-line", "apt"
     ],
@@ -54,12 +62,16 @@ platform_to_tag = {
         "aiohttp", "python", "django"
     ]
 }
+#test
+platform_to_tag = {
+    "askubuntu": ["apt"]
+}
 
 PAGE_MAX = 3 #number of pages to consider
-tag_urls = {}
 
 
-def main():
+def collect_urls():
+    tag_urls = {}
     for platform in platform_to_tag:
         tags = platform_to_tag[platform]
         for tag in tags:
@@ -68,7 +80,23 @@ def main():
                 for url in Tag_To_Url(base=platform, tag=tag, page=i):
                     tag_urls[tag].extend(url)
             print(f"Found {len(tag_urls[tag])} urls for tag {tag} in {platform} searching {PAGE_MAX} pages")
+    return tag_urls
 
 
-Link_To_Data(['https://askubuntu.com/questions/17823/how-to-list-all-installed-packages'])
+def data_from_url(tag_urls: dict):
+    dataset = []
+    for tag in tag_urls:
+        urls = tag_urls[tag]
+        for url in urls:
+            dataset.append(Url_To_Data(url=url))
+        print(f"Collected data from {len(urls)} urls with tag {tag}")
+    return dataset
+
+
+if __name__ == '__main__':
+    urls = collect_urls()
+    dataset = data_from_url(tag_urls=urls)
+
+
+
 
